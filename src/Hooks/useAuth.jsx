36ceb3c9d4b19e8d6e145c4288322550d7fe2e3e
Tushar12152 <tusharimran470@gmail.com>
2023-11-24 12:04:17 +0600

    useAuth hook added and auth provider added

---
+++ src/Hooks/useAuth.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+
+
+
+const useAuth = () => {
+
+    const result=useContext(AuthContext)
+
+    return result
+}
+
+export default useAuth;
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,25 +1,58 @@
-import {  createContext } from "react";
+import {  createContext, useEffect, useState } from "react";
+import { PropTypes } from 'prop-types';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../Config/firebase.config";
 
 
 export const AuthContext=createContext(null)
 
 const AuthProvider = ({children}) => {
 
+    const [loading,setLoading]=useState(true)
+    const [user,setUser]=useState(null)
+
+
+    const createUser=(email,password)=>{
+         setLoading(true)
+         return createUserWithEmailAndPassword(auth,email,password)
+    }
+
+    const SignIn=(email,password)=>{
+        setLoading(true)
+           return signInWithEmailAndPassword(auth,email,password)
+    }
+
+
+    useEffect(()=>{
+           const unsubscribe=onAuthStateChanged(auth,currentUser=>{
+               setUser(currentUser)
+               setLoading(false)
+           })
+
+           return ()=>{
+               return unsubscribe()
+           }
+    },[])
 
 
 
     const authInfo={
-
+        loading,
+        user,
+        createUser,
+        SignIn
     }
     return (
         <AuthContext.Provider value={authInfo}>
-              {Children}
+              {children}
         </AuthContext.Provider>
     );
 };
 
 
 
-AuthProvider.
+AuthProvider.propTypes={
+     children:PropTypes.node,
+}
 
 export default AuthProvider;
