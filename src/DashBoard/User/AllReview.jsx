d844178420c93a24307c3ef44af970aa6b8153a9
Tushar12152 <tusharimran470@gmail.com>
2023-11-27 18:30:10 +0600

    sort in decending order upto likes

--- src/DashBoard/User/AllReview.jsx
+++ src/DashBoard/User/AllReview.jsx
@@ -5,135 +5,94 @@ import { FaEye, FaTrash } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import swal from "sweetalert";
 
-
 const AllReview = () => {
-const axiosSecure=useAxiosSecure()
+  const axiosSecure = useAxiosSecure();
 
+  const { data: reviews = [], refetch } = useQuery({
+    queryKey: ['review'],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/reviews`)
+      return res.data
+    }
+  });
 
-    const { data:reviews=[],refetch } = useQuery({
-        queryKey: ['review'],
-        queryFn: async () =>{
-            const res=await axiosSecure.get(`/reviews`)
-    
-            return res.data
-        }    
-      })
-
-
-      const handleDelete=(id)=>{
-
-
-
-        swal({
-            title: "Are you sure?",
-            text: "You want to Delete this Review?",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-          })
-          .then((willDelete) => {
-            if (willDelete) {
-            
-    
-    
-                axiosSecure.delete(`/reviews/${id}`)
-                .then(res=>{
-                    if(res.data.deletedCount>0){
-                      swal(" This Review Is deleted", {icon: "success", });
-      
-                      refetch()
-                    }
-              })
-    
-    
-    
-    
-           
-            
-            
-            .catch((error) => {
-                console.log(error);
-                swal("Error", "Failed to delete", "error");
-              });
-    
-    
-            } else {
-              swal("This Review is safe!");
+  const handleDelete = (id) => {
+    swal({
+      title: "Are you sure?",
+      text: "You want to Delete this Review?",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        axiosSecure.delete(`/reviews/${id}`)
+          .then(res => {
+            if (res.data.deletedCount > 0) {
+              swal(" This Review Is deleted", { icon: "success" });
+              refetch();
             }
+          })
+          .catch((error) => {
+            console.log(error);
+            swal("Error", "Failed to delete", "error");
           });
-    
-    
-    
-    
-    
-    
-    
-         
+      } else {
+        swal("This Review is safe!");
       }
-    
+    });
+  };
 
-
-    return (
-        <div>
-            <Title heading={'All Review'}></Title>
-
-
-
-            <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>
-          #
-        </th>
-        <th>Meal Title</th>
-        <th>Likes</th>
-        <th>Reviews</th>
-        <th>Delete</th>
-        <th>View Details</th>
-      </tr>
-    </thead>
-    <tbody>
-      {reviews.map((review,idx)=><tr key={review._id}>
-        <th>
-          {idx+1}
-        </th>
-        <td>
-          <div className="flex items-center gap-3">
-          
-            <div>
-              <div className="font-bold">{review?.Title}</div>
-              
-            </div>
-          </div>
-        </td>
-        <td>
-         {review?.likes}
-        </td>
-        <td>{review?.review}</td>
-        <th>
-          <button onClick={()=>handleDelete(review?._id)} className="btn  bg-[#f76042] text-white"><FaTrash></FaTrash></button>
-        </th>
-        <th>
-       <Link to={`/detail/${review?.detailId}`}>  
-        <button  className="btn  bg-[#f76042] text-white"><FaEye></FaEye></button></Link>
-        </th>
-      </tr>)}
-      
-     
-    </tbody>
   
-    
-  </table>
-</div>
-   
+  const sortedReviews = reviews.slice().sort((a, b) => a.likes - b.likes);
 
-     
-
-            
-        </div>
-    );
+  return (
+    <div>
+      <Title heading={'All Review'}></Title>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Meal Title</th>
+              <th>Likes</th>
+              <th>Reviews</th>
+              <th>Delete</th>
+              <th>View Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedReviews.map((review, idx) => (
+              <tr key={review._id}>
+                <th>{idx + 1}</th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div>
+                      <div className="font-bold">{review?.Title}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>{review?.likes}</td>
+                <td>{review?.review}</td>
+                <th>
+                  <button onClick={() => handleDelete(review?._id)} className="btn bg-[#f76042] text-white">
+                    <FaTrash />
+                  </button>
+                </th>
+                <th>
+                  <Link to={`/detail/${review?.detailId}`}>
+                    <button className="btn bg-[#f76042] text-white">
+                      <FaEye />
+                    </button>
+                  </Link>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllReview;
