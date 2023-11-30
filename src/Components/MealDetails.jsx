cd4c280ac5601f862af8951d35f5bcd3d9ac2222
Tushar12152 <tusharimran470@gmail.com>
2023-11-30 10:28:03 +0600

    react awesome button implement in View detail page see all button

--- src/Components/MealDetails.jsx
+++ src/Components/MealDetails.jsx
@@ -1,6 +1,8 @@
 import { Link, useLoaderData } from "react-router-dom";
 import Container from "../Layout/Container";
 import MealDetailCard from "./MealDetailCard";
+import { AwesomeButton } from 'react-awesome-button';
+import 'react-awesome-button/dist/styles.css';
 
 const MealDetails = () => {
     const data=useLoaderData()
@@ -13,7 +15,16 @@ const MealDetails = () => {
                  <MealDetailCard meal={data}></MealDetailCard>
 
                 <div className="flex  py-10 justify-center">
-                <Link to='/meals' className="btn bg-[#f76042] text-white">See All</Link>
+                <Link to='/meals'>
+
+                   <AwesomeButton 
+                    type="anchor"
+                    className="custom-awesome-btn"
+                    style={{ backgroundColor: "#f76042", color: "white" }}>
+                        see all
+                   </AwesomeButton>
+                   </Link>
+
                 </div>
           </Container>
     );
